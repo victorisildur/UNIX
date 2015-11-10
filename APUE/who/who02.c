8b680d438855eef19932889481616952108116fe
victorisildur <victorisildur@163.com>
2015-11-10 11:11:17 +0800

    add ut_type

--- APUE/who/who02.c
+++ APUE/who/who02.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 
 void show_info(struct utmpx * utbufp);
+void show_time(long time_sec);
 
 int main()
 {
@@ -16,5 +17,15 @@ void show_info(struct utmpx * utbufp)
 {
     printf("%s ", utbufp->ut_user);
     printf("%s ", utbufp->ut_id);
+    printf("%s ", utbufp->ut_line);
+    printf("%d ", utbufp->ut_type);
+    show_time(utbufp->ut_tv.tv_sec);
     printf("%s \n", utbufp->ut_host);
+}
+
+void show_time(long time_sec)
+{
+    char * cp;
+    cp = ctime(&time_sec);
+    printf("%s", cp+4);
 }
