7f5fe2ca17fefea8aad58a378c59ac27567589d3
victorisildur <victorisildur@163.com>
2015-11-12 20:15:51 +0800

    pong str game

---
+++ APUE/game/pong_str.c
@@ -0,0 +1,30 @@
+#include <curses.h>
+
+#define LEFTEDGE  10
+#define RIGHTEDGE 30
+#define ROW       10
+
+main()
+{
+    char message[] = "Hello";
+    char blank[]   = "     ";
+    int dir = 1;
+    int pos = LEFTEDGE;
+
+    initscr();
+    clear();
+    while(1) {
+        move(ROW, pos);
+        addstr(message);
+        move(LINES-1, COLS-1);
+        refresh();
+        sleep(1);
+        move(ROW, pos);
+        addstr(blank);
+        pos += dir;
+        if (pos >= RIGHTEDGE)
+            dir = -1;
+        if (pos <= LEFTEDGE)
+            dir = 1;
+    }
+}
