cfba258e8bf6536ad33eceb519b298a1bcd656b3
victorisildur <victorisildur@163.com>
2015-11-15 23:00:12 +0800

    process start

---
+++ APUE/process/exec01.c
@@ -0,0 +1,10 @@
+int main()
+{
+    char * arglist[3];
+    arglist[0] = "ls";
+    arglist[1] = "-l";
+    arglist[2] = 0;
+    printf("*** about to exec ls -l\n");
+    execvp("ls", arglist);
+    printf("*** ls is done.\n");
+}
